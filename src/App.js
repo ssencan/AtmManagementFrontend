475b2a56133c8ffbfcd8ed1d8423b398c06c9543
ssencan <s.sencan03@hotmail.com>
2023-07-20 16:03:57 +0300

    AddedAxios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import { Button, IconButton, TextField } from "@mui/material";
+import * as colors from "@mui/material/colors";
+import SendIcon from "@mui/icons-material/Send";
+import axios from "axios";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [displayValue, setDisplayValue] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAxiosRequest = async () => {
+    try {
+      const response = await axios.get("https://localhost:44334/api/Atm");
+      console.log(response.data);
+      setDisplayValue(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (inputValue === "") {
+      alert("No input was entered");
+    } else if (window.confirm("Are you sure you want to submit?")) {
+      setDisplayValue([inputValue]);
+      console.log(inputValue);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextField
+        id="textField1"
+        label="Enter Input"
+        variant="filled"
+        style={{ backgroundColor: colors.common.white }}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <IconButton
+        aria-label="delete"
+        style={{ backgroundColor: colors.amber[600], color: colors.teal[500] }}
+        onClick={handleButtonClick}
+      >
+        <SendIcon />
+      </IconButton>
+      <Button variant="contained" color="primary" onClick={handleAxiosRequest}>
+        Send Request
+      </Button>
+      <div>
+        {displayValue.map((item, index) => (
+          <Fragment key={index}>
+            <p>{item.id}</p>
+            <p>{item.atmName}</p>
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
