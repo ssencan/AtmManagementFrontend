49c0a16b44920d3269898fbcbf4a49c6df4a55ed
ssencan <s.sencan03@hotmail.com>
2023-07-22 01:17:57 +0300

    addedAddModal

--- src/App.js
+++ src/App.js
@@ -1,65 +1,58 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Button, IconButton, TextField } from "@mui/material";
 import * as colors from "@mui/material/colors";
 import SendIcon from "@mui/icons-material/Send";
 import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [displayValue, setDisplayValue] = useState([]);
+  const [atmData, setAtmData] = useState([]);
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleAxiosRequest = async () => {
-    try {
-      const response = await axios.get("https://localhost:44334/api/Atm");
-      console.log(response.data);
-      setDisplayValue(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleButtonClick = () => {
-    if (inputValue === "") {
-      alert("No input was entered");
-    } else if (window.confirm("Are you sure you want to submit?")) {
-      setDisplayValue([inputValue]);
-      console.log(inputValue);
-    }
-  };
+  useEffect(() => {
+    const fetchAtmData = async () => {
+      try {
+        const response = await axios.get("https://localhost:44334/api/Atm");
+        setAtmData(response.data);
+      } catch (atmGETerror) {
+        console.error(atmGETerror);
+      }
+    };
+    fetchAtmData();
+  }, []);
 
   return (
-    <div>
-      <TextField
-        id="textField1"
-        label="Enter Input"
-        variant="filled"
-        style={{ backgroundColor: colors.common.white }}
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-      <IconButton
-        aria-label="delete"
-        style={{ backgroundColor: colors.amber[600], color: colors.teal[500] }}
-        onClick={handleButtonClick}
-      >
-        <SendIcon />
-      </IconButton>
-      <Button variant="contained" color="primary" onClick={handleAxiosRequest}>
-        Send Request
-      </Button>
-      <div>
-        {displayValue.map((item, index) => (
-          <Fragment key={index}>
-            <p>{item.id}</p>
-            <p>{item.atmName}</p>
-          </Fragment>
-        ))}
-      </div>
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ATM Name</TableCell>
+            <TableCell>Latitude</TableCell>
+            <TableCell>Longitude</TableCell>
+            <TableCell>City Name</TableCell>
+            <TableCell>District Name</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {atmData.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.atmName}</TableCell>
+              <TableCell>{item.latitude}</TableCell>
+              <TableCell>{item.longitude}</TableCell>
+              <TableCell>{item.cityName}</TableCell>
+              <TableCell>{item.districtName}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
