8f8f8c172ccbb0f106c61578b40f3bddbdf15856
ssencan <s.sencan03@hotmail.com>
2023-07-24 13:23:44 +0300

    addedUpdateModal

--- src/App.js
+++ src/App.js
@@ -49,9 +49,17 @@ const DeleteButton = styled(Button)({
   },
 });
 
+const UpdateButton = styled(Button)({
+  backgroundColor: "#008000", // yeşil
+  "&:hover": {
+    backgroundColor: "#006400", // üzerine gelindiğinde daha koyu bir yeşil
+  },
+});
+
 function App() {
   const [atmData, setAtmData] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
   const [atm, setAtm] = useState({
     id: "",
     atmName: "",
@@ -89,7 +97,7 @@ function App() {
       });
       alert("ATM added successfully.");
       fetchAtmData();
-      handleClose();
+      handleCloseAdd();
     } catch (atmPOSTerror) {
       console.error(atmPOSTerror);
     }
@@ -103,19 +111,46 @@ function App() {
       if (userConfirmation) {
         await axios.delete(`https://localhost:44334/api/Atm/${id}`);
         alert("ATM deleted successfully.");
-        await fetchAtmData();
+        fetchAtmData();
       }
     } catch (atmPOSTerror) {
       console.error(atmPOSTerror);
     }
   };
-
-  const handleOpen = () => {
-    setOpen(true);
+  const updateAtm = async () => {
+    try {
+      await axios.put("https://localhost:44334/api/Atm/UpdateAtm", {
+        id: atm.id,
+        atmName: atm.atmName,
+        latitude: parseFloat(atm.latitude),
+        longitude: parseFloat(atm.longitude),
+        cityID: parseInt(atm.cityID),
+        districtID: parseInt(atm.districtID),
+        isActive: atm.isActive,
+      });
+      alert("ATM updated successfully.");
+      fetchAtmData();
+      handleCloseUpdate();
+    } catch (atmPuterror) {
+      console.error(atmPuterror);
+    }
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleOpenAdd = () => {
+    setOpenAdd(true);
+  };
+
+  const handleOpenUpdate = (atmToUpdate) => {
+    setAtm(atmToUpdate);
+    setOpenUpdate(true);
+  };
+
+  const handleCloseAdd = () => {
+    setOpenAdd(false);
+  };
+
+  const handleCloseUpdate = () => {
+    setOpenUpdate(false);
   };
 
   const handleInputChange = (e) => {
@@ -145,7 +180,7 @@ function App() {
                   <Button
                     variant="contained"
                     color="primary"
-                    onClick={handleOpen}
+                    onClick={handleOpenAdd}
                   >
                     Add ATM
                   </Button>
@@ -168,13 +203,21 @@ function App() {
                       Delete
                     </DeleteButton>
                   </StyledTableCell>
+                  <StyledTableCell>
+                    <UpdateButton
+                      variant="contained"
+                      onClick={() => handleOpenUpdate(item)}
+                    >
+                      Update
+                    </UpdateButton>
+                  </StyledTableCell>
                 </StyledTableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
 
-        <Dialog label="Add" open={open} onClose={handleClose}>
+        <Dialog label="Add" open={openAdd} onClose={handleCloseAdd}>
           <DialogTitle>Add New ATM</DialogTitle>
           <DialogContent>
             <DialogContentText>Enter ATM details.</DialogContentText>
@@ -221,11 +264,67 @@ function App() {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose} color="primary">
+            <Button onClick={handleCloseAdd} color="primary">
               Cancel
             </Button>
             <Button onClick={addAtm} color="primary">
               Add
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <Dialog label="Update" open={openUpdate} onClose={handleCloseUpdate}>
+          <DialogTitle>Update ATM</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Enter ATM details.</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="atmName"
+              label="ATM Name"
+              type="text"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="latitude"
+              label="Latitude"
+              type="text"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="longitude"
+              label="Longitude"
+              type="text"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="cityID"
+              label="City ID"
+              type="text"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="districtID"
+              label="District ID"
+              type="text"
+              fullWidth
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseUpdate} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={updateAtm} color="primary">
+              Update
             </Button>
           </DialogActions>
         </Dialog>
