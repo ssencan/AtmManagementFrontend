619972ed34c52b86163d8b3e2a4eb49983b576d0
ssencan <s.sencan03@hotmail.com>
2023-07-27 12:01:47 +0300

    Added React leaflet map api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import {
   Button,
   Dialog,
@@ -24,11 +32,33 @@ import {
   FormControl,
   InputLabel,
 } from "@mui/material";
-import axios from "axios";
-import * as yup from "yup";
-import { useFormik } from "formik";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+const MapComponent = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 const CustomSelect = ({ label, name, value, onChange, children, disabled }) => (
   <FormControl fullWidth>
@@ -257,6 +287,23 @@ function App() {
 
   return (
     <Fragment>
+      <div>
+        <MapContainer
+          center={[40.989891586469625, 28.71540046609974]}
+          zoom={13}
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[40.989891586469625, 28.71540046609974]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
       <ToastContainer />
       <AppBar position="static">
         <Toolbar>
@@ -480,4 +527,5 @@ function App() {
     </Fragment>
   );
 }
+
 export default App;
