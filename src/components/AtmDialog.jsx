e0fad026e9bc23e642246206e34e30098b0fd30a
ssencan <s.sencan03@hotmail.com>
2023-08-03 12:17:07 +0300

    Added dialog component

---
+++ src/components/AtmDialog.jsx
@@ -0,0 +1,249 @@
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { useFormik } from "formik";
+import { toast, ToastContainer } from "react-toastify";
+import * as yup from "yup";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  styled,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  Button,
+  MenuItem,
+  Grid,
+} from "@mui/material";
+
+const CustomSelect = ({
+  label,
+  name,
+  value,
+  onChange,
+  children,
+  disabled,
+  style,
+}) => (
+  <FormControl
+    fullWidth
+    style={{ marginBottom: "5px", marginTop: "10px", ...style }}
+  >
+    <InputLabel id={`${name}-label`}>{label}</InputLabel>
+    <Select
+      labelId={`${name}-label`}
+      name={name}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      MenuProps={{
+        PaperProps: {
+          style: {
+            maxHeight: "230px",
+            width: "20ch",
+          },
+        },
+      }}
+    >
+      {children}
+    </Select>
+  </FormControl>
+);
+
+const atmValidationSchema = yup.object({
+  atmName: yup.string("Enter ATM Name").required("ATM Name is required"),
+  latitude: yup
+    .number("Enter valid Latitude")
+    .required("Latitude is required")
+    .min(-90, "Minimum latitude is -90")
+    .max(90, "Maximum latitude is 90"),
+  longitude: yup
+    .number("Enter valid Longitude")
+    .required("Longitude is required")
+    .min(-180, "Minimum longitude is -180")
+    .max(180, "Maximum longitude is 180"),
+  cityID: yup.number("Select City").required("City is required"),
+  districtID: yup.number("Select District").required("District is required"),
+});
+
+const AtmDialog = ({
+  open,
+  setOpen,
+  mode,
+  initialValues,
+  fetchCities,
+  fetchDistricts,
+  cities,
+  districts,
+  fetchAtmData,
+  setCities,
+  setDistricts,
+}) => {
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: atmValidationSchema,
+    onSubmit: (values) => {
+      if (mode === "add") {
+        addAtm(values);
+      } else if (mode === "update") {
+        updateAtm(values);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (formik.values.cityID) {
+      fetchDistricts(formik.values.cityID);
+    }
+  }, [formik.values.cityID]);
+
+  const addAtm = async () => {
+    try {
+      await axios.post("https://localhost:44334/api/Atm/CreateAtm", {
+        atmName: formik.values.atmName,
+        latitude: parseFloat(formik.values.latitude),
+        longitude: parseFloat(formik.values.longitude),
+        cityID: parseInt(formik.values.cityID),
+        districtID: parseInt(formik.values.districtID),
+        isActive: formik.values.isActive,
+      });
+      toast.success("ATM added successfully.");
+      fetchAtmData();
+      handleClose();
+    } catch (atmPOSTerror) {
+      console.error(atmPOSTerror);
+      toast.error("Failed to add ATM.");
+    }
+  };
+
+  const updateAtm = async () => {
+    try {
+      const response = await axios.put(
+        "https://localhost:44334/api/Atm/UpdateAtm",
+        {
+          id: formik.values.id,
+          atmName: formik.values.atmName,
+          latitude: parseFloat(formik.values.latitude),
+          longitude: parseFloat(formik.values.longitude),
+          cityID: parseInt(formik.values.cityID),
+          districtID: parseInt(formik.values.districtID),
+          isActive: formik.values.isActive,
+        }
+      );
+      console.log("Update response: ", response);
+      await fetchAtmData();
+      toast.success("ATM updated successfully.");
+      handleClose();
+    } catch (atmPuterror) {
+      console.error(atmPuterror);
+      toast.error("Failed to update ATM.");
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    formik.resetForm();
+    setCities([]);
+    setDistricts([]);
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{mode === "add" ? "Add New ATM" : "Update ATM"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Enter ATM details.</DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="atmName"
+          name="atmName"
+          label="ATM Name"
+          type="text"
+          fullWidth
+          value={formik.values.atmName}
+          onChange={formik.handleChange}
+          error={formik.touched.atmName && Boolean(formik.errors.atmName)}
+          helperText={formik.touched.atmName && formik.errors.atmName}
+        />
+
+        <TextField
+          margin="dense"
+          name="latitude"
+          label="Latitude"
+          type="text"
+          fullWidth
+          value={formik.values.latitude}
+          onChange={formik.handleChange}
+          error={formik.touched.latitude && Boolean(formik.errors.latitude)}
+          helperText={formik.touched.latitude && formik.errors.latitude}
+        />
+        <TextField
+          margin="dense"
+          name="longitude"
+          label="Longitude"
+          type="text"
+          fullWidth
+          value={formik.values.longitude}
+          onChange={formik.handleChange}
+          error={formik.touched.longitude && Boolean(formik.errors.longitude)}
+          helperText={formik.touched.longitude && formik.errors.longitude}
+        />
+
+        <CustomSelect
+          label="City"
+          name="cityID"
+          value={formik.values.cityID}
+          onChange={async (e) => {
+            formik.handleChange(e);
+            await fetchDistricts(e.target.value);
+          }}
+        >
+          {cities.map((city) => (
+            <MenuItem key={city.id} value={city.id}>
+              {city.name}
+            </MenuItem>
+          ))}
+        </CustomSelect>
+
+        <CustomSelect
+          label="District"
+          name="districtID"
+          value={formik.values.districtID}
+          onChange={formik.handleChange}
+          disabled={!formik.values.cityID}
+        >
+          {districts.map((district) => (
+            <MenuItem key={district.id} value={district.id}>
+              {district.name}
+            </MenuItem>
+          ))}
+        </CustomSelect>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={formik.handleSubmit} color="primary">
+          {mode === "add" ? "Add" : "Update"}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AtmDialog;
--- src/components/AtmTable.jsx
+++ src/components/AtmTable.jsx
@@ -4,6 +4,7 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AtmDialog from "../components/AtmDialog";
 
 import {
   Table,
@@ -28,40 +29,6 @@ import {
   MenuItem,
   Grid,
 } from "@mui/material";
-
-const CustomSelect = ({
-  label,
-  name,
-  value,
-  onChange,
-  children,
-  disabled,
-  style,
-}) => (
-  <FormControl
-    fullWidth
-    style={{ marginBottom: "5px", marginTop: "10px", ...style }}
-  >
-    <InputLabel id={`${name}-label`}>{label}</InputLabel>
-    <Select
-      labelId={`${name}-label`}
-      name={name}
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-      MenuProps={{
-        PaperProps: {
-          style: {
-            maxHeight: "230px",
-            width: "20ch",
-          },
-        },
-      }}
-    >
-      {children}
-    </Select>
-  </FormControl>
-);
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   "&.MuiTableCell-head": {
@@ -107,57 +74,33 @@ const UpdateButton = styled(Button)({
   boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .8)",
 });
 
-const atmValidationSchema = yup.object({
-  atmName: yup.string("Enter ATM Name").required("ATM Name is required"),
-  latitude: yup
-    .number("Enter valid Latitude")
-    .required("Latitude is required")
-    .min(-90, "Minimum latitude is -90")
-    .max(90, "Maximum latitude is 90"),
-  longitude: yup
-    .number("Enter valid Longitude")
-    .required("Longitude is required")
-    .min(-180, "Minimum longitude is -180")
-    .max(180, "Maximum longitude is 180"),
-  cityID: yup.number("Select City").required("City is required"),
-  districtID: yup.number("Select District").required("District is required"),
-});
-
 const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
   const [openAdd, setOpenAdd] = useState(false);
   const [openUpdate, setOpenUpdate] = useState(false);
   const [cities, setCities] = useState([]);
   const [districts, setDistricts] = useState([]);
-  const formik = useFormik({
-    //bunu galiba home içine taşıycaz. Çünkü hem table da handleopenupdate ile hem de modal componentinde kullanılacak
-    initialValues: {
-      id: "",
-      atmName: "",
-      latitude: "",
-      longitude: "",
-      cityID: "",
-      districtID: "",
-      isActive: true,
-    },
-    validationSchema: atmValidationSchema,
-    onSubmit: (values) => {
-      if (openAdd) {
-        addAtm(values); //burda formikle add çağrılıyor. addmetodunu modal componentinde mi açmam lazım?
-      } else if (openUpdate) {
-        updateAtm(values);
-      }
-    },
+  const [open, setOpen] = useState(false);
+  const [mode, setMode] = useState("add");
+  const [atmToUpdate, setAtmToUpdate] = useState({});
+  const [initialValues, setInitialValues] = useState({
+    id: "",
+    atmName: "",
+    latitude: "",
+    longitude: "",
+    cityID: "",
+    districtID: "",
+    isActive: true,
   });
 
   useEffect(() => {
     fetchCities();
   }, []);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     if (formik.values.cityID) {
       fetchDistricts(formik.values.cityID);
     }
-  }, [formik.values.cityID]);
+  }, [formik.values.cityID]); */
 
   const fetchCities = async () => {
     try {
@@ -180,7 +123,7 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
     }
   };
 
-  const addAtm = async () => {
+  /* const addAtm = async () => {
     try {
       await axios.post("https://localhost:44334/api/Atm/CreateAtm", {
         atmName: formik.values.atmName,
@@ -197,7 +140,7 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
       console.error(atmPOSTerror);
       toast.error("Failed to add ATM.");
     }
-  };
+  }; */
 
   const deleteAtm = async (id) => {
     try {
@@ -214,7 +157,7 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
       toast.error("Failed to delete ATM.");
     }
   };
-  const updateAtm = async () => {
+  /*  const updateAtm = async () => {
     try {
       const response = await axios.put(
         "https://localhost:44334/api/Atm/UpdateAtm",
@@ -236,35 +179,36 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
       console.error(atmPuterror);
       toast.error("Failed to update ATM.");
     }
-  };
+  }; */
 
-  const resetForm = () => {
-    formik.resetForm();
-    setCities([]);
-    setDistricts([]);
-  };
-
-  const handleCloseAdd = () => {
-    setOpenAdd(false);
+  /*  const handleClose = () => {
+    setOpen(false);
     resetForm();
-  };
+  }; */
 
-  const handleCloseUpdate = () => {
+  /*   const handleCloseUpdate = () => {
     setOpenUpdate(false);
     resetForm();
+  }; */
+
+  const handleOpenDialog = async (mode, atmToUpdate = null) => {
+    setMode(mode);
+    if (mode === "update") {
+      setAtmToUpdate(atmToUpdate);
+      // await fetchCities();
+      // await fetchDistricts(atmToUpdate.cityID);
+      // formik.setValues(atmToUpdate);
+    }
+    setOpen(true);
   };
 
-  const handleOpenAdd = async () => {
-    await Promise.all([fetchCities(), fetchDistricts()]);
-    setOpenAdd(true);
-  };
-
-  const handleOpenUpdate = async (atmToUpdate) => {
-    await fetchCities();
-    await fetchDistricts(atmToUpdate.cityID);
-    formik.setValues(atmToUpdate);
+  /*   const handleOpenUpdate = async (atmToUpdate) => {
+    //await fetchCities();
+    //await fetchDistricts(atmToUpdate.cityID);
+    //formik.setValues(atmToUpdate);
+    setAtmToUpdate(atmToUpdate);
     setOpenUpdate(true);
-  };
+  }; */
 
   return (
     <div>
@@ -278,7 +222,10 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
               <StyledTableCell>City Name</StyledTableCell>
               <StyledTableCell>District Name</StyledTableCell>
               <StyledTableCell colSpan={6}>
-                <AddButton variant="contained" onClick={handleOpenAdd}>
+                <AddButton
+                  variant="contained"
+                  onClick={() => handleOpenDialog("add")}
+                >
                   Add ATM
                 </AddButton>
               </StyledTableCell>
@@ -307,7 +254,7 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
                 <StyledTableCell>
                   <UpdateButton
                     variant="contained"
-                    onClick={() => handleOpenUpdate(item)}
+                    onClick={() => handleOpenDialog("update", item)}
                   >
                     Update
                   </UpdateButton>
@@ -317,163 +264,19 @@ const AtmTable = ({ atmData, hoveredAtmId, setHoveredAtmId, fetchAtmData }) => {
           </TableBody>
         </Table>
       </TableContainer>
-
-      <Dialog label="Add" open={openAdd} onClose={handleCloseAdd}>
-        <DialogTitle>Add New ATM</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Enter ATM details.</DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="atmName"
-            name="atmName"
-            label="ATM Name"
-            type="text"
-            fullWidth
-            value={formik.values.atmName}
-            onChange={formik.handleChange}
-            error={formik.touched.atmName && Boolean(formik.errors.atmName)}
-            helperText={formik.touched.atmName && formik.errors.atmName}
-          />
-
-          <TextField
-            margin="dense"
-            name="latitude"
-            label="Latitude"
-            type="text"
-            fullWidth
-            value={formik.values.latitude}
-            onChange={formik.handleChange}
-            error={formik.touched.latitude && Boolean(formik.errors.latitude)}
-            helperText={formik.touched.latitude && formik.errors.latitude}
-          />
-          <TextField
-            margin="dense"
-            name="longitude"
-            label="Longitude"
-            type="text"
-            fullWidth
-            value={formik.values.longitude}
-            onChange={formik.handleChange}
-            error={formik.touched.longitude && Boolean(formik.errors.longitude)}
-            helperText={formik.touched.longitude && formik.errors.longitude}
-          />
-
-          <CustomSelect
-            label="City"
-            name="cityID"
-            value={formik.values.cityID}
-            onChange={async (e) => {
-              formik.handleChange(e);
-              await fetchDistricts(e.target.value);
-            }}
-          >
-            {cities.map((city) => (
-              <MenuItem key={city.id} value={city.id}>
-                {city.name}
-              </MenuItem>
-            ))}
-          </CustomSelect>
-
-          <CustomSelect
-            label="District"
-            name="districtID"
-            value={formik.values.districtID}
-            onChange={formik.handleChange}
-            disabled={!formik.values.cityID}
-          >
-            {districts.map((district) => (
-              <MenuItem key={district.id} value={district.id}>
-                {district.name}
-              </MenuItem>
-            ))}
-          </CustomSelect>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseAdd} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={formik.handleSubmit} color="primary">
-            Add
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      <Dialog label="Update" open={openUpdate} onClose={handleCloseUpdate}>
-        <DialogTitle>Update ATM</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Enter ATM details.</DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            name="atmName"
-            label="ATM Name"
-            type="text"
-            fullWidth
-            value={formik.values.atmName}
-            onChange={formik.handleChange}
-            error={formik.touched.atmName && Boolean(formik.errors.atmName)}
-            helperText={formik.touched.atmName && formik.errors.atmName}
-          />
-          <TextField
-            margin="dense"
-            name="latitude"
-            label="Latitude"
-            type="text"
-            fullWidth
-            value={formik.values.latitude}
-            onChange={formik.handleChange}
-            error={formik.touched.latitude && Boolean(formik.errors.latitude)}
-            helperText={formik.touched.latitude && formik.errors.latitude}
-          />
-          <TextField
-            margin="dense"
-            name="longitude"
-            label="Longitude"
-            type="text"
-            fullWidth
-            value={formik.values.longitude}
-            onChange={formik.handleChange}
-            error={formik.touched.longitude && Boolean(formik.errors.longitude)}
-            helperText={formik.touched.longitude && formik.errors.longitude}
-          />
-          <CustomSelect
-            label="City"
-            name="cityID"
-            value={formik.values.cityID}
-            onChange={async (e) => {
-              formik.handleChange(e);
-              await fetchDistricts(e.target.value);
-            }}
-          >
-            {cities.map((city) => (
-              <MenuItem key={city.id} value={city.id}>
-                {city.name}
-              </MenuItem>
-            ))}
-          </CustomSelect>
-          <CustomSelect
-            label="District"
-            name="districtID"
-            value={formik.values.districtID}
-            onChange={formik.handleChange}
-          >
-            {districts.map((district) => (
-              <MenuItem key={district.id} value={district.id}>
-                {district.name}
-              </MenuItem>
-            ))}
-          </CustomSelect>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseUpdate} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={formik.handleSubmit} color="primary">
-            Update
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <AtmDialog
+        open={open}
+        setOpen={setOpen}
+        mode={mode}
+        initialValues={initialValues}
+        fetchCities={fetchCities}
+        fetchDistricts={fetchDistricts}
+        cities={cities}
+        setCities={setCities}
+        setDistricts={setDistricts}
+        districts={districts}
+        fetchAtmData={fetchAtmData}
+      />
     </div>
   );
 };
